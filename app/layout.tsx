70f06384fd64224c247126ae28a654df0769b4d1
Annas <53283862+Annas-Dev@users.noreply.github.com>
2024-07-08 22:55:43 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Annas | Personal Portfolio",
+  description: "Annas is a software enginer with 4 years of experience.",
 };
 
 export default function RootLayout({
